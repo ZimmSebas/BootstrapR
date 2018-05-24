054301d6f204e9c3cb4bd2eb767cb7ec09c710f6
ZimmSebas <zimmermannsebas@gmail.com>
2018-05-24 18:03:38 -0300

    Avances en medias y samples
    Intentos de media por columna, logros en samples

--- Bootstrap1.r
+++ Bootstrap1.r
@@ -1,23 +1,27 @@
 set.seed(140)
 
 arr <- c(3,6,9)
-p <- c(0.2,0.5,0.3)
+p <- c(0.15,0.5,0.35)
 n <- c(4)
 k <- c(20)
 b1 <- c(1000)
 b2 <- c(50000)
 
-test <- c()
-test
+
+samples <- replicate(k, sample(arr,size=n,prob=p,replace=TRUE)) 
+samples
+
+media <- array (1:k)
+
+
 
 for (i in 0:k){
-   test[length(test)+1] = sample(arr,size=n,prob=p,replace=TRUE)
-   test
-  media <- mean(test[i])
+
+  media[i] <- rowMeans(samples, na.rm = FALSE, dims = 1)
   media
   for (j in 0:n){
-    test[i][j] <- test[i][j] - media
+    samples[i][j] <- samples[i][j] - media
   }
 }
 
-test
+samples
