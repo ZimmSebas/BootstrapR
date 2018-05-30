5b8e6ddc036727f35531fab67c9590c0895fa511
ZimmSebas <zimmermannsebas@gmail.com>
2018-05-30 16:23:07 -0300

    Version Alpha 1.1
    Agrego el manejo de arrays en forma correcta, queda por verse la paralelización

--- Bootstrap.r
+++ Bootstrap.r
@@ -7,36 +7,36 @@ k <- c(20)
 b1 <- c(1000)
 b2 <- c(50000)
 a<-sqrt(n/(n-1))
-mediamuestral <- array(1:b1)
+mediamuestral <- array(1:b2)
 
 
-for (l in 1:b1){
+
+
+
+for (l in 1:b2){
   samples <- replicate(k, sample(arr,size=n,prob=p,replace=TRUE)) 
-
+  
   mediaTotal <- mean(samples)
   
   mediaCol <- array (1:k)
   
   mediaCol <- colMeans(samples, dims = 1)
   
-  for (i in 0:(n-1)){
+  for (i in 1:n){
     for (j in 1:k){
-      samples[(i*k)+j] <- samples[(i*k)+j] - mediaCol[j]
+      samples[i,j] <- samples[i,j] - mediaCol[j]
     }
   }
   
-  
-  
   boots <- sample(samples,size=n,replace=TRUE)
-  
   
   for (i in 1:n){
     boots[i] <- mediaTotal + boots[i]*a
   }
-  
   mediamuestral[l] <- mean(boots)
 }
 
 mediamuestral <- sort(mediamuestral)
+print("Termine")
 
 print(mediamuestral)
