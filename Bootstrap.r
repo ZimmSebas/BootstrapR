5ae2fd30642531749fab52329a627ec78dd76018
ZimmSebas <zimmermannsebas@gmail.com>
2018-06-01 18:10:09 -0300

    Alpha 1.2
    Mejora de muestras, falta organizar y agregar cuestion de muestra media muestral y percentiles

--- Bootstrap.r
+++ Bootstrap.r
@@ -4,39 +4,56 @@ arr <- c(3,6,9)
 p <- c(0.15,0.5,0.35)
 n <- c(4)
 k <- c(20)
-b1 <- c(1000)
+b1 <- c(100)
 b2 <- c(50000)
 a<-sqrt(n/(n-1))
-mediamuestral <- array(1:b2)
+mediamuestral <- array(1:2000)
+
+
+limiteInf <- array(1:b1)
+limiteSup <- array(1:b1)
 
 
 
-
-
-for (l in 1:b2){
+for (l in 1:b1){
+  
   samples <- replicate(k, sample(arr,size=n,prob=p,replace=TRUE)) 
-  
+    
   mediaTotal <- mean(samples)
-  
+    
   mediaCol <- array (1:k)
-  
+    
   mediaCol <- colMeans(samples, dims = 1)
-  
+    
   for (i in 1:n){
     for (j in 1:k){
       samples[i,j] <- samples[i,j] - mediaCol[j]
     }
   }
+
+  boots <- replicate(100,samples)
+
+  perm <- sample(boots,size=8000)
   
-  boots <- sample(samples,size=n,replace=TRUE)
   
-  for (i in 1:n){
-    boots[i] <- mediaTotal + boots[i]*a
+  for (i in 0:1999){
+    mediamuestral[i+1] = ((perm[(4*i)+1]+perm[(4*i)+2]+perm[(4*i)+3]+perm[(4*i)+4])/4)
+    mediamuestral[i+1] = mediamuestral[i+1]*a + mediaTotal 
   }
-  mediamuestral[l] <- mean(boots)
+  
+  mediamuestral <- sort(mediamuestral)
+  
+  limiteInf[l] <- mediamuestral[50]
+  limiteSup[l] <- mediamuestral[1950]
+
 }
 
-mediamuestral <- sort(mediamuestral)
-print("Termine")
+print(limiteInf)
+print(limiteSup)
 
-print(mediamuestral)
+#hist(samples)
+#hist(perm)
+
+#for (i in 1:n){
+#  boots[i] <- mediaTotal + boots[i]*a
+#}
