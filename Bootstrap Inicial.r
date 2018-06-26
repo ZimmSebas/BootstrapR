735e5538a5de57985203d9a509939674b170639a
ZimmSebas <zimmermannsebas@gmail.com>
2018-06-25 21:34:46 -0300

    Agrego el Primer Bootstrap por las dudas
    Nada que agregar más que este agregado

---
+++ Bootstrap Inicial.r
@@ -0,0 +1,42 @@
+set.seed(140)
+
+arr <- c(3,6,9)
+p <- c(0.15,0.5,0.35)
+n <- c(4)
+k <- c(20)
+b1 <- c(1000)
+b2 <- c(50000)
+a<-sqrt(n/(n-1))
+mediamuestral <- array(1:b1)
+
+
+for (l in 1:b1){
+  samples <- replicate(k, sample(arr,size=n,prob=p,replace=TRUE)) 
+
+  mediaTotal <- mean(samples)
+  
+  mediaCol <- array (1:k)
+  
+  mediaCol <- colMeans(samples, dims = 1)
+  
+  for (i in 0:(n-1)){
+    for (j in 1:k){
+      samples[(i*k)+j] <- samples[(i*k)+j] - mediaCol[j]
+    }
+  }
+  
+  
+  
+  boots <- sample(samples,size=n,replace=TRUE)
+  
+  
+  for (i in 1:n){
+    boots[i] <- mediaTotal + boots[i]*a
+  }
+  
+  mediamuestral[l] <- mean(boots)
+}
+
+mediamuestral <- sort(mediamuestral)
+
+print(mediamuestral)
